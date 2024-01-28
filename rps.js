637471b8d143daf8c8e0c5ea7f376bf1cbb0df95
Jacob Ye <gmding777@gmail.com>
2024-01-28 12:05:16 -0800

    Fixed playRound logic

--- rps.js
+++ rps.js
@@ -5,10 +5,11 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     let playerChoice = playerSelection.toLowerCase();
+    console.log(`${playerChoice} ${computerSelection}`);
     if (playerChoice == "rock" && computerSelection == "paper") {
         console.log("You Lose! Paper beats Rock!");
         return 1;
-    } else if (computerSelection == "scissors") {
+    } else if (playerChoice == "rock" && computerSelection == "scissors") {
         console.log("You win! Rock beats Scissors!");
         return -1;
     }
@@ -16,43 +17,29 @@ function playRound(playerSelection, computerSelection) {
     if (playerChoice == "paper" && computerSelection == "scissors") {
         console.log("You Lose! Scissors beats Paper!");
         return 1;
-    } else if (computerSelection == "rock") {
+    } else if (playerChoice == "paper" && computerSelection == "rock") {
         console.log("You win! Paper beats Rock!");
         return -1;
     }
 
-    // We already know that the player choice is scissors (assuming they played by the rules)
-    // If this was a real life app I would first validate their choices.
-    if (computerSelection == "rock") {
+    if (playerChoice == "scissors" && computerSelection == "rock") {
         console.log("You Lose! Rock beats Scissors!");
         return 1;
-    } else if (computerSelection == "paper") {
+    } else if (playerChoice == "scissors" && computerSelection == "paper") {
         console.log("You win! Scissors beats Paper!");
         return -1;
     }
 
     //replay during tie;
-    console.log("It's a tie!");
-    playRound(playerSelection, computerSelection);
-}
-
-function game() {
-    let count = 0;
-    for (let i = 0; i < 5; i++) {
-        count += playRound(prompt("Rock, papers, or scissors?", "rock"), getComputerChoice());
-    }
-    if (count > 2) {
-        console.log("You beat the computer!");
-    } else {
-        console.log("You lost against the computer!");
-    }
+    console.log("It's a tie! Playing again.");
+    playRound(playerSelection, getComputerChoice());
 }
 
 const buttons = document.querySelectorAll('button');
 console.log(buttons);
 buttons.forEach((button) => {
     console.log("reached");
-    button.addEventListener('click', function(e) {
-        console.log(e.target);
+    button.addEventListener('click', (e) => {
+        playRound(e.target.innerHTML, getComputerChoice());
     });
 });
